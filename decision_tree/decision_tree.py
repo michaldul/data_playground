81d89a757e44e574c3151b3b33864501a3950be4
Michał <michaldul@outlook.com>
2017-06-11 09:30:59 +0200

    gini criterion implemented

--- decision_tree/decision_tree.py
+++ decision_tree/decision_tree.py
@@ -18,15 +18,27 @@ def calc_entropy(y):
     return entropy
 
 
-def information_gain(parent, child_a, child_b):
+def calc_gini(y):
     """
-    >>> information_gain(np.array([1, 2, 3]), np.array([1, 2]), np.array([3])) > 0
+    >>> calc_gini(np.array([1, 2, 2, 2]))
+    0.375
+    """
+    gini = 1
+    for cls in np.unique(y):
+        prob = (y == cls).sum() / len(y)
+        gini -= prob ** 2
+    return gini
+
+
+def information_gain(parent, child_a, child_b, criterion):
+    """
+    >>> information_gain(np.array([1, 2, 3]), np.array([1, 2]), np.array([3]), calc_entropy) > 0
     True
-    >>> information_gain(np.array([1, 1, 2, 2]), np.array([1, 2]), np.array([1, 2])) < 0
+    >>> information_gain(np.array([1, 1, 2, 2]), np.array([1, 2]), np.array([1, 2]), calc_gini) < 0
     True
     """
-    children_entropy = len(child_a) / len(parent) * calc_entropy(child_a) \
-                       + len(child_b) * len(parent) * calc_entropy(child_b)
+    children_entropy = len(child_a) / len(parent) * criterion(child_a) \
+                       + len(child_b) * len(parent) * criterion(child_b)
     return calc_entropy(parent) - children_entropy
 
 
@@ -39,8 +51,10 @@ def find_dominant_class(y):
 
 
 class DecisionTree:
-    def __init__(self):
-        pass
+    def __init__(self, max_depth=3, criterion=calc_entropy):
+        self.tree = None
+        self.max_depth = max_depth
+        self.criterion = criterion
 
     def _generate_splits(self, X, y, depth):
         if depth == 1:
@@ -49,7 +63,7 @@ class DecisionTree:
         info_gains = {}
         for feature in X.columns:
             split = X[feature].mean()
-            info_gains[feature] = information_gain(y, y[X[feature] < split], y[X[feature] >= split])
+            info_gains[feature] = information_gain(y, y[X[feature] < split], y[X[feature] >= split], self.criterion)
 
         best_feature = max(info_gains, key=info_gains.get)
         best_split = X[best_feature].mean()
@@ -60,8 +74,8 @@ class DecisionTree:
                 'left_node': self._generate_splits(X[split_mask], y[split_mask], depth - 1),
                 'right_node': self._generate_splits(X[~split_mask], y[~split_mask], depth - 1)}
 
-    def fit(self, X, y, max_depth=3):
-        self.tree = self._generate_splits(X, y, max_depth)
+    def fit(self, X, y):
+        self.tree = self._generate_splits(X, y, self.max_depth)
         print(self.tree)
         return self
 
@@ -84,7 +98,7 @@ if __name__ == '__main__':
     y = iris['class']
     X_train, X_test, y_train, y_test = train_test_split(X, y)
 
-    cls = DecisionTree().fit(X_train, y_train, max_depth=4)
+    cls = DecisionTree(max_depth=4, criterion=calc_entropy).fit(X_train, y_train)
     predictions = cls.predict(X_test)
 
-    print(accuracy_score(y_test, predictions)) # 0.894736842105
+    print(accuracy_score(y_test, predictions))  # 0.894736842105
